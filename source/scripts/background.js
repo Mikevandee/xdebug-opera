33afafdc20cfa5d1882f09ff65935f4a67533df8
Mike van Dee <mike.van.dee@avrotros.nl>
2018-01-30 11:52:50 +0100

    [WIP] Added command

--- source/scripts/background.js
+++ source/scripts/background.js
@@ -1,7 +1,13 @@
 (function (XDebug) {
     var xDebug = new XDebug();
 
-    chrome.browserAction.onClicked.addListener(function() {
+    chrome.browserAction.onClicked.addListener(function () {
         xDebug.toggleSession();
+    });
+
+    chrome.commands.onCommand.addListener(function (command) {
+        if (command == 'toggle_debug_action') {
+            xDebug.toggleSession();
+        }
     });
 })(XDebug);
